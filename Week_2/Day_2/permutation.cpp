e05620af6282426618577dd6701d0faceb49abaf
3215tanu <3215.tanu@gmail.com>
2026-06-09 22:41:11 +0530

    Add permutation checking function in permutation.cpp

---
+++ Week_2/Day_2/permutation.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        int n = s1.length();
+        sort(s1.begin(), s1.end());
+
+        for (int i = 0; i <= s2.length() - n; i++) {
+
+            string temp = s2.substr(i, n);
+            sort(temp.begin(), temp.end());
+
+            if (temp == s1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
