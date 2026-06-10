c6674f03113f2df3622c71436c8588600f3f676a
3215tanu <3215.tanu@gmail.com>
2026-06-10 22:12:32 +0530

    Add repeatedSubstringPattern function in substring.cpp
    Implement a function to check for repeated substring patterns in a string.

---
+++ Week_2/Day_3/substring.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n = s.length();
+
+        for (int i = 1; i <= n / 2; i++) {
+
+            if (n % i== 0) {
+
+                string sub = s.substr(0, i);
+                string temp = "";
+
+                for (int j = 0; j< n / i; j++) {
+                    temp += sub;
+                }
+
+                if (temp == s) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
