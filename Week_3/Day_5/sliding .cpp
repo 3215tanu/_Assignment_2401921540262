c8d776c3fa70a5039fbc1afd0f32a1e162a6e55b
3215tanu <3215.tanu@gmail.com>
2026-06-19 21:50:28 +0530

    Refactor maxSlidingWindow to use priority queue

--- Week_3/Day_5/sliding .cpp
+++ Week_3/Day_5/sliding .cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        priority_queue<pair<int, int>> pq;
         vector<int> ans;
 
-        for (int i = 0; i <= nums.size() - k; i++) {
-            int mx = nums[i];
+        for (int i = 0; i < nums.size(); i++) {
+            pq.push({nums[i], i});
+            if (i >= k - 1) {
+                while (pq.top().second <= i - k) {
+                    pq.pop();
+                }
 
-            for (int j = i; j < i + k; j++) {
-                mx = max(mx, nums[j]);
+                ans.push_back(pq.top().first);
             }
-
-            ans.push_back(mx);
         }
 
         return ans;
