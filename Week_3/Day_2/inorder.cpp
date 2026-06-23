b9199c70aec17b37942520d896d7bee237f63a04
3215tanu <3215.tanu@gmail.com>
2026-06-23 22:40:58 +0530

    Implement inorder traversal for binary tree

---
+++ Week_3/Day_2/inorder.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        inorder(root, ans);
+        return ans;
+    }
+
+    void inorder(TreeNode* node, vector<int>& ans) {
+        if (node == nullptr) return;
+
+        inorder(node->left, ans);
+        ans.push_back(node->val);
+        inorder(node->right, ans);
+    }
+};
